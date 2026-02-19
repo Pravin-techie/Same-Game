0f9e5438567798183a31877ac4c4f0c53ee35765
VIDHYADHARANRP7777 <padmavathirajendran772@gmail.com>
2026-02-20 00:06:09 +0530

    Added comments to Dynamic_Programming.py
    Added comments to functions for clarity.

--- Dynamic_Programming.py
+++ Dynamic_Programming.py
@@ -1,4 +1,6 @@
-# FUNCTION 2: DP SCORE DIFFERENCE - Turn-aware optimal evaluation
+# FUNCTION 2: DP SCORE DIFFERENCE - Turn-aware optimal evaluation 
+# Worst case:  O(2^N × N²)
+
 def dp_score_difference(grid, memo, is_cpu_turn):
     """
     Returns maximum score DIFFERENCE (current player - opponent)
@@ -46,6 +48,9 @@ def dp_score_difference(grid, memo, is_cpu_turn):
         return worst
 
 # FUNCTION 3: CPU MOVE - Using turn-aware adversarial DP
+#Worst case: O(N² + S × N²)
+
+
 def cpu_best_move(grid):
     """
     CPU MOVE USING Divide & Conquer + DP
